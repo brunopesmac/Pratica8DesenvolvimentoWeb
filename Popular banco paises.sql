853561ed8d5a025c1d3128ce386bba56854215ad
brunopesmac <62313565+brunopesmac@users.noreply.github.com>
2020-05-04 19:38:47 -0300

    Add files via upload

---
+++ Popular banco paises.sql
@@ -0,0 +1,5 @@
+USE Paises;
+INSERT INTO `Pais` (`id`,`nome`,`populacao`,`area`) VALUES (1,'Brasil',209.3,8511000);
+INSERT INTO `Pais` (`id`,`nome`,`populacao`,`area`) VALUES (2,'Estados Unidos',327.2,9834000);
+INSERT INTO `Pais` (`id`,`nome`,`populacao`,`area`) VALUES (3,'Portugal',10.29,92212);
+INSERT INTO `Pais` (`id`,`nome`,`populacao`,`area`) VALUES (4,'Canada',37.59,9985000);
